d70d07f27663cd648f6c9cb6251e0568855fec66
Venkatesh-Prasad Ranganath <rvprasad@ksu.edu>
2019-01-14 12:28:37 -0600

    Uses Java NIO to optimize intermediate file ops

--- single-node-multi-core/src/main/scala/sorter/parallel/Sorter.scala
+++ single-node-multi-core/src/main/scala/sorter/parallel/Sorter.scala
@@ -9,11 +9,17 @@
 
 package sorter.parallel
 
-import java.io.{BufferedWriter, File, FileWriter, PrintWriter}
+import java.io._
+import java.nio.ByteBuffer
 
 import scala.collection.mutable
 import scala.io.Source
 import scala.util.{Failure, Sorting, Success, Try}
+
+private object Sorter {
+  val LONG_IN_BYTES = 8
+  val NUM_OF_LONGS = 4096
+}
 
 class Sorter(val parallelism: Int, val numOfInts: Int,
              val tempDirName: String) {
@@ -21,29 +27,37 @@ class Sorter(val parallelism: Int, val numOfInts: Int,
 
   def sort(inFile: String, outFile: String): Try[Unit] = {
     try {
-      implicit val ordering = Ordering.by { x:Long => x }
       val sortedChunkFiles = Source.fromFile(inFile)
         .getLines.map(_.toLong)
         .grouped(numOfInts).withPartial(true).map(_.toArray)
         .grouped(parallelism).withPartial(true).map { chunks =>
-          chunks.par.foreach { Sorting.quickSort(_) }
+          chunks.par.foreach { Sorting.quickSort(_)(Ordering.Long) }
           val tmp1 = combineSortedChunksIntoIterator(chunks.map(_.iterator))
           val file = java.io.File.createTempFile("sort", "chunk", tempDir)
-          file.deleteOnExit()
-          val writer = new PrintWriter(new BufferedWriter(
-            new FileWriter(file)))
-          try
-            tmp1.foreach(writer.println)
-          finally
-            writer.close()
+          writeLongsInto(tmp1, file)
           file
-        }
-      val ret = combineSortedChunksIntoFile(sortedChunkFiles, outFile)
+        }.toSeq
+      val ret = combineSortedChunksIntoFile(sortedChunkFiles.iterator, outFile)
       sortedChunkFiles.foreach(_.delete)
       ret
     } catch {
       case e: Exception => e.printStackTrace() ; Failure(e)
     }
+  }
+
+  private def writeLongsInto(tmp1: Iterator[Long], file: File): Unit = {
+    val fs = new FileOutputStream(file)
+    val fc = fs.getChannel
+    val bb = ByteBuffer.allocate(Sorter.NUM_OF_LONGS * Sorter.LONG_IN_BYTES)
+    try
+      tmp1.grouped(Sorter.NUM_OF_LONGS).foreach { g =>
+        bb.clear
+        g.foreach(bb.putLong)
+        bb.flip
+        fc.write(bb)
+      }
+    finally
+      fs.close()
   }
 
   private def combineSortedChunksIntoIterator(
@@ -55,11 +69,11 @@ class Sorter(val parallelism: Int, val numOfInts: Int,
     val heap = new mutable.PriorityQueue[(Long, Iterator[Long])]()(Ordering.by(-_._1))
     heap ++= tmp1.map { a => (a.next, a) }
 
-    return new Iterator[Long] {
-      override def hasNext = heap.nonEmpty
+    new Iterator[Long] {
+      override def hasNext: Boolean = heap.nonEmpty
 
-      override def next = {
-        val (elem, chunk) = heap.dequeue()
+      override def next: Long = {
+        val (elem, chunk) = heap.dequeue
         if (chunk.hasNext)
           heap += ((chunk.next, chunk))
         elem
@@ -71,15 +85,34 @@ class Sorter(val parallelism: Int, val numOfInts: Int,
                                           outFile: String): Try[Unit] = {
     val sink = new PrintWriter(new File(outFile))
     try {
-      val sources = sortedChunkFiles.map(Source.fromFile).toSeq
+      val sources = sortedChunkFiles.map(new FileInputStream(_))
       try {
-        val seqOfLines = sources.map(_.getLines.map(_.toLong))
+        val seqOfLines = sources.map(readLongsFrom).toSeq
         combineSortedChunksIntoIterator(seqOfLines).foreach(sink.println)
         Success(())
       } finally
-        sources.foreach(_.close)
+        sources.foreach(_.close())
     } finally
       sink.close()
+  }
+
+  private def readLongsFrom(fs: FileInputStream): Iterator[Long] = {
+    val fc = fs.getChannel
+    val bb = ByteBuffer.allocate(Sorter.NUM_OF_LONGS * Sorter.LONG_IN_BYTES)
+    new Iterator[Long] {
+      bb.limit(0)
+
+      override def hasNext: Boolean = bb.hasRemaining || fs.available > 0
+
+      override def next: Long = {
+        if (!bb.hasRemaining) {
+          bb.clear
+          fc.read(bb)
+          bb.flip
+        }
+        bb.getLong
+      }
+    }
   }
 }
 
